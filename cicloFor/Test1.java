9f6a21e2a487967dc013b9ba6c97f365a4803c32
David <davidpachecoask@gmail.com>
2020-09-17 04:24:22 -0500

    Ciclo for

---
+++ cicloFor/Test1.java
@@ -0,0 +1,28 @@
+package cicloFor;
+
+/**
+ *
+ * @author David
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //Variable entera
+        int numeroMinimo = 0;
+        //ciclo for se compone de:
+        //i se debe inicializar
+        //i puede incrementar o decrementar
+        for (int i = 20; i >= numeroMinimo; i--) {
+            //Muestra el numero incrementado
+            System.out.println("Numero: " + i);
+        }
+        //Fin...
+
+    }
+
+}
